f8e1afdb0619928dd88a6770b2e0b2f92eac5046
Lorenzo Stacchio <lorenzo.stacchio.dev@gmail.com>
2021-01-30 11:39:02 +0100

    Implement shape filter

--- GoogleImageScraper_Efficient.py
+++ GoogleImageScraper_Efficient.py
@@ -12,7 +12,7 @@ import json
 
 class GoogleImageScraper():
     
-    def __init__(self,webdriver_path,image_path, search_key,number_of_images, screen_width, screen_height,similar_images = False, link_similar_image=None,color=None, headless=True):
+    def __init__(self,webdriver_path,image_path, search_key,number_of_images, screen_width, screen_height,similar_images = False, link_similar_image=None,color=None, shape=None, headless=True):
         self.search_key = search_key
         self.number_of_images = number_of_images
         self.webdriver_path = webdriver_path
@@ -21,20 +21,22 @@ class GoogleImageScraper():
         self.screen_width = screen_width
         self.screen_height = screen_height
         self.color = color
+        self.shape = shape
         self.similar_image = similar_images
         self.link_similar_image = link_similar_image
         #self.url = "https://www.google.com/search?q=%s&tbm=isch&hl=it&tbs&rlz=1C1UEAD_itIT929IT929&sa=X&ved=0CAEQpwVqFwoTCIDosdGzt-4CFQAAAAAdAAAAABAC&biw=%s&bih=%s"%(search_key,self.screen_width, self.screen_height)
-        self.url = "https://www.google.com/search?q=%s&tbm=isch&hl=it&tbs&rlz=1C1UEAD_itIT929IT929&sa=X&ved=0CAEQpwVqFwoTCIDosdGzt-4CFQAAAAAdAAAAABAC&biw=%s&bih=%s"%(search_key,self.screen_width, self.screen_height)
-        print(self.url)
+        self.base_url = "https://www.google.com/search?q=%s&tbm=isch&hl=it&tbs&rlz=1C1UEAD_itIT929IT929&sa=X&ved=0CAEQpwVqFwoTCIDosdGzt-4CFQAAAAAdAAAAABAC&biw=%s&bih=%s"%(search_key,self.screen_width, self.screen_height)
         # Init all this variables with init_parameters()
         self.time_wait_between_dowloads,self.time_wait_between_scrolling,self.timeout_for_download_images = None,None,None 
         # color variables 
-        self.color_list, self.black_and_white, self.transparent = None,None,None 
+        self.color_list,self.colorized, self.black_and_white, self.transparent = None,None,None,None
         self.chars_to_clean_urls = None # chars to replace in urls
+        self.shapes = None
         # Load configs and parse arguments
         config_dict = self.load_config()
         self.init_parameters(config_dict)
-        self.parse_arguments()
+        self.url = self.parse_arguments()
+        print("parsing", self.url)
         self.driver = self.init_driver()
 
     def load_config(self):
@@ -51,22 +53,41 @@ class GoogleImageScraper():
         self.time_wait_between_scrolling = config_dict["time"]["time_wait_between_scrolling"]
         self.timeout_for_download_images = config_dict["time"]["timeout_for_download_images"]
         self.color_list = config_dict["image_params"]["colors"]["normal"]
+        self.colorized = config_dict["image_params"]["colors"]["colorized"]
         self.transparent = config_dict["image_params"]["colors"]["trasparent_code"]
         self.black_and_white = config_dict["image_params"]["colors"]["black_and_white"]
+        self.shapes = config_dict["image_params"]["shapes"]
         self.chars_to_clean_urls = config_dict["chars_to_clean_urls"]
+
 
     def parse_arguments(self):
         color_params_parsing = ""
-        if self.color in self.color_list:
-            self.url = self.url.replace("tbs", "tbs=ic:specific%2Cisc:" + str(self.color))
-        elif self.color == self.transparent:
-            self.url = self.url.replace("tbs", "tbs=ic:trans")
-        elif self.color == self.black_and_white:
-            self.url = self.url.replace("tbs", "tbs=ic:gray")
-        elif self.similar_image: # other type of research
-            self.url = "https://www.google.it/searchbyimage?image_url=%s&encoded_image=&image_content=&filename=&hl=it"%(link_similar_image)
-            self.url = self.get_url_similar_images_google(self.url)
-
+        # PARSE FOR FIRST SIMILAR
+        if self.similar_image: # other type of research
+            url = "https://www.google.it/searchbyimage?image_url=%s&encoded_image=&image_content=&filename=&hl=it"%(link_similar_image)
+            return self.get_url_similar_images_google(url)
+        else: #normal search
+            # tra un parametro e l'altro ci va la virgola ---> &tbs=ic:color,iar:t
+            # parsing color
+            color_string = None
+            if self.color in self.color_list:
+                color_string = "ic:specific%2Cisc:" + str(self.color)
+                #self.url = self.url.replace("tbs", "tbs=ic:specific%2Cisc:" + str(self.color))
+            elif self.color == self.colorized:
+                color_string = "ic:" + self.colorized
+            elif self.color == self.transparent:
+                color_string = "ic:" + self.transparent
+                #self.url = self.url.replace("tbs", "tbs=ic:trans")
+            elif self.color == self.black_and_white:
+                color_string = "ic:" + self.black_and_white
+                #self.url = self.url.replace("tbs", "tbs=ic:gray")
+            # parsing shape
+            shape_string = None
+            if self.shape in self.shapes.keys():
+                shape_string = "iar:" + self.shapes[self.shape]
+            "square", "wide", "panoramic"
+            final_string_filter = "tbs=" + ','.join([filter  for filter in [color_string,shape_string] if filter])
+            return self.base_url.replace("tbs", str(final_string_filter))
 
     def get_url_similar_images_google(self,url):
         #TO DO
